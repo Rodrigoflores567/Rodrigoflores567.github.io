714413d92863ec34178540289b0ae4cfd83fa8c3
Rodrigoflores567 <oio.ioio7772003@gmail.com>
2023-03-20 11:33:50 -0600

    Paquete Biología
    Paquete con los tres juegos

---
+++ Paquete_biologia/Leyenda/Leyenda_resources/js/ardoraColoreaCFG.js
@@ -0,0 +1,36 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=360; timeIni=360; timeBon=0;
+var successes=0; successesMax=0; attempts=0; attemptsMax=1;
+var score=0; scoreMax=1; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=false;
+var tiTimeType=0;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=false;
+var tiAttempts=false;
+var tiScore=false;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="Lo coneguiste!"; messageTime=""; messageError="Lo puedes hacer mejor"; messageErrorG="Lo puedes hacer mejor"; messageAttempts=""; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var wordsGame="TGV5ZW5kYQ=="; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var joinPar=[];
+var c=["#0080C0","#800040","#8080FF","#00FF00","#FF0000","#FF8040","#004000"];
+var x=["354","348","257","147","200","98","90"];
+var y=["36","263","193","292","105","49","175"];
+var t=["5","9","15","19","8","3","4"];
+var cM=["#0080C0","#800040","#8080FF","#00FF00","#004000","#FF8040","#FF0000"];
+var tM=["2+3","2+7","7+8","9+10","2+2","1+2","4+4"];
+var indexColor=0;
